b6ebfee654e21d3fd4d6e2a54580fd197feeeb32
Rene <racosta323@gmail.com>
2024-03-02 14:43:24 -0600

    adds menu items to helpers

--- lib/cli.py
+++ lib/cli.py
@@ -1,36 +1,28 @@
 #!/usr/bin/env python3
 from helpers import (
     greeting,
-    exit_program 
+    exit_program,
+    menu,
+    user_menu,
+    friends_menu
 )
 
 def main():
-    while True:
-        # greeting()
-        menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            user_menu()
-        elif choice == "2":
-            friends_menu()
-        else:
-            print("Invalid choice -- enter number corresponding to menu item")
+    # greeting()
+    # while True:
+    menu()
+    choice = input("> ")
+    if choice == "0":
+        exit_program()
+    elif choice == "1":
+        user_menu()
+    elif choice == "2":
+        friends_menu()
+    else:
+        print("Invalid choice -- enter number corresponding to menu item")
 
 
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Where have you been?")
-    print("2. Where have your friends been?")
-
-def user_menu():
-    print("hello user")
- 
-
-def friends_menu():
-    print("hello friends")     
+    
              
 if __name__ == '__main__':
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,12 +1,55 @@
+from cli import main
+from trip import Trip
+
 def greeting():
     name = input("What is your name? ")
     age = input("How old are you? ")
-    while name and age:
-        print(f'Hello, {name}! Nice to meet you.')
-        name = False
-        
+    print(f'Hello, {name}! Nice to meet you.')
 
+        
 def exit_program():
     print("Until next time, Traveler!")
     exit()
 
+def menu():
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. Where have you been?")
+    print("2. Where have your friends been?")
+
+def user_menu():
+    print("1. Enter a record of a location you've been to")
+    print("2. Show my locations.")
+    choice = input("> ")
+    if choice == "1":
+        enter_location()
+    if choice == "2":
+        pass
+
+def enter_location():    
+    print("What city have you been to? (Enter a location.)")
+    city = input("> ")
+    print("In what state?")
+    state = input("> ")
+    print("Out of 5, how many stars would you give it? (1: Meh to 5:Yeah!)")
+    stars = input("> ")
+    print("Cool. Thanks. Your entry has been recorded.")
+    main()
+
+def show_locations_menu():
+    print("1. Show all locations I've been to.")
+    print("2. Show locations I've been to by country.")
+    print("3. Show locations I've been to by state.")
+    print("4. Show locations I've been to with X stars")
+
+def show_all_loc_menu():
+    print("1. Sort by date of visit.")
+    print("2. Sort alphabetically by state.")
+    print("3. Sort alphabetically by country.")
+    
+
+def friends_menu():
+    print("Where have your friends been? THIS WILL GIVE ALL.")
+    print("Where have friends older than you been?")
+    print("Where have friends younger than you been?")
+    print("How many friends have been to where I've been?")  
