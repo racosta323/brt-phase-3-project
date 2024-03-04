85380c9f499cb4d2f1ba932d511bb97ba00c6cd3
Rene <racosta323@gmail.com>
2024-03-04 16:59:59 -0600

    added class methods and prop attributes

--- lib/debug.py
+++ lib/debug.py
@@ -9,18 +9,18 @@ from __init__ import CONN, CURSOR
 
 #datetime requires year,month,date: https://www.w3schools.com/python/python_datetime.asp
 
-trip1 = Trip( "March", 2023)
+# trip1 = Trip( "March", 2023)
 
 
 # from traveler: def __init__(self, full_name, id, age):
-tr1 = Traveler("Rene", 1, 35)
-tr2 = Traveler("Keya", 2, 25)
+# tr1 = Traveler("Rene", 1, 35)
+# tr2 = Traveler("Keya", 2, 25)
 
 l1 = Location("Johnsonville", "South Carolina", "United States", 2)
 l2 = Location("Austin", "TX", "US", 1)
 
-trip1 = Trip.create_instance("March", 2020, 2, 1, 2)
-trip2 = Trip.create_instance("April", 2019, 2, 2, 1)
+# trip1 = Trip.create_instance("March", 2020, 2, 1, 2)
+# trip2 = Trip.create_instance("April", 2019, 2, 2, 1)
 
 # Trip.get_all_by_name("Rene")
 ipdb.set_trace()
